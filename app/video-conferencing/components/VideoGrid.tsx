2bc7d21550c3c1cf326b74b3fac938b7c4f11ff1
chiboycalix <igwechinonso77@gmail.com>
2025-01-07 13:08:45 +0100

    working stage 4

--- app/video-conferencing/components/VideoGrid.tsx
+++ app/video-conferencing/components/VideoGrid.tsx
@@ -8,9 +8,9 @@ const VideoGrid = ({ localUser, remoteUsers, StreamPlayer }: any) => {
       case 1:
         return "grid-cols-1";
       case 2:
-        return "grid-cols-1 md:grid-cols-2";
+        return "grid-cols-2";
       case 3:
-        return "grid-cols-2 md:grid-cols-2";
+        return "grid-cols-4";
       case 4:
         return "grid-cols-2 md:grid-cols-2";
       default:
@@ -18,51 +18,52 @@ const VideoGrid = ({ localUser, remoteUsers, StreamPlayer }: any) => {
     }
   };
 
-  const getVideoContainerClass = (isLocal: any, index: number) => {
+  const getVideoContainerClass = (isLocal: boolean, index: number, totalParticipants: number) => {
     if (totalParticipants === 1) {
       return "col-span-1 row-span-1 h-full";
     }
 
     if (totalParticipants === 2) {
-      return "col-span-1 h-96";
+      return "col-span-1 h-80";
     }
 
-    if (totalParticipants === 3 && isLocal) {
-      return "col-span-2 md:col-span-1 h-96";
+    if (totalParticipants === 3) {
+      if (index < 2) {
+        return "col-span-2 h-80";
+      }
+      return "col-span-2 col-start-2 h-80";
     }
 
     return "col-span-1 h-64 md:h-72";
   };
 
+  const remoteUsersArray = Object.entries(remoteUsers || {}).map(([uid, user]: any) => ({
+    ...user,
+    uid
+  }));
+
+  const allParticipants = [
+    { ...localUser, isLocal: true },
+    ...remoteUsersArray
+  ];
+
   return (
     <div className="h-full w-full">
-      <div className={`grid ${getGridLayout()} gap-2 p-2 h-full`}>
-        {/* Local User Video */}
-        <div className={getVideoContainerClass(true, 0)}>
-          <div className="relative h-full w-full rounded-lg overflow-hidden">
-            <StreamPlayer
-              videoTrack={localUser.videoTrack || null}
-              audioTrack={localUser.audioTrack || null}
-              uid={localUser.uid || ""}
-            />
-            <div className="absolute bottom-2 left-2 bg-black/50 px-2 py-1 rounded text-white text-sm">
-              You
-            </div>
-          </div>
-        </div>
-
-        {/* Remote Users */}
-        {Object.entries(remoteUsers || {}).map(([uid, user]: any, index) => (
-          user.videoTrack && (
-            <div key={uid} className={getVideoContainerClass(false, index + 1)}>
+      <div className={`grid ${getGridLayout()} gap-2 p-2 h-full ${totalParticipants === 2 ? 'items-center' : ''}`}>
+        {allParticipants.map((participant, index) => (
+          participant.videoTrack && (
+            <div
+              key={participant.uid}
+              className={getVideoContainerClass(participant.isLocal, index, totalParticipants)}
+            >
               <div className="relative h-full w-full rounded-lg overflow-hidden">
                 <StreamPlayer
-                  videoTrack={user.videoTrack}
-                  audioTrack={user.audioTrack}
-                  uid={uid}
+                  videoTrack={participant.videoTrack}
+                  audioTrack={participant.audioTrack}
+                  uid={participant.uid}
                 />
                 <div className="absolute bottom-2 left-2 bg-black/50 px-2 py-1 rounded text-white text-sm">
-                  {user.name || `User ${index + 1}`}
+                  {participant.isLocal ? 'You' : participant.name || `User ${index}`}
                 </div>
               </div>
             </div>
